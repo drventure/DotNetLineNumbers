ce3d0fcffe497c0eaeba94fb5e77ff1c2e5fb491
Darin Higgins <dhiggins@worksoft.com>
2018-09-24 11:27:05 -0500

    Added t4 template to automatically bump the version number.

--- VersionInfo/SharedAssemblyInfo.cs
+++ VersionInfo/SharedAssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
-// Shared Assembly info for GenerateLineMap Projects
+// Shared Assembly info for All Projects
 #region "Static Assembly Version info
 [assembly: AssemblyCompany("Darin Higgins")]
 [assembly: AssemblyProduct("GenerateLineMap")]
-[assembly: AssemblyCopyright("Copyright © 2018 Darin Higgins")]
+[assembly: AssemblyCopyright("Copyright � 2018 Darin Higgins")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 #if DEBUG
@@ -20,16 +21,8 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
 [assembly: AssemblyVersion(VersionInfo.Version)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
 //this attrib supports semantic versioning
@@ -42,6 +35,5 @@ using System.Runtime.InteropServices;
 /// </summary>
 internal struct VersionInfo
 {
-	public const string Version = "0.0.0.11";
+    public const string Version = "0.0.0.13";
 }
-
